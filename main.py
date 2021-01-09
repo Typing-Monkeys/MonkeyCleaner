e0ff1eb71ef145d61cfbf1534768a8447088458d
Zecown <44830740+NicoloPosta@users.noreply.github.com>
2021-01-09 19:17:23 +0100

    Aggiunti Commenti

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ from sklearn.model_selection import train_test_split
 import numpy as np
 import pandas as pd
 import time
-import matplotlib.pyplot as plt
+
+
+# Legge e prepara il Dataset per il Training
 def prepare_data(file_name:str) -> dict:
     print("Importo il dataset ...")
     start_time = time.time()
@@ -27,6 +29,7 @@ def prepare_data(file_name:str) -> dict:
     return (x, None, y, None)
 
 
+# Legge un'immagine da file e la prepara per essere usata come elemento per il Testing
 def prepare_test(file_name, bw=True):
     test = cv2.imread(file_name, cv2.IMREAD_GRAYSCALE)
 
@@ -43,6 +46,7 @@ def prepare_test(file_name, bw=True):
     return np.array([test])
 
 
+'''
 def prova(file):
     img = cv2.imread(file)
     
@@ -77,26 +81,33 @@ def prova(file):
     cv2.imshow('SESSO', img2)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
-    
+'''
 
-def prova2(file):
+
+# Prende un immagine (presunta tabella) da file e 
+# ne estrae le celle
+def monkeyRead(file, print=False):
+    # Importa 2 volte l'immagine per modificarne una
+    ## per dopo, importare 2 immagini potrebbe essere inutile...
     im1 = cv2.imread(file, 0)
     im = cv2.imread(file)
 
     ret, thresh_value = cv2.threshold(im1, 180, 255, cv2.THRESH_BINARY_INV)
 
-    kernel = np.ones((5,5), np.uint8)
-    dilated_value = cv2.dilate(thresh_value,kernel, iterations = 1)
+    kernel = np.ones((5, 5), np.uint8)
+    dilated_value = cv2.dilate(thresh_value,kernel, iterations=1)
 
     contours, hierarchy = cv2.findContours(dilated_value, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    
     cordinates = []
     for cnt in contours:
         x,y,w,h = cv2.boundingRect(cnt)
         cordinates.append((x,y,w,h))
-        #bounding the images
-        
+
+        # diesegna un rettangolo sopra ogni elemento trovato      
         im = cv2.rectangle(im,(x,y),(x+w,y+h),(0,0,255),1)
 
+    # stampa l'immagine con i rettangoli
     cv2.imshow('SESSO', im)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
@@ -104,7 +115,7 @@ def prova2(file):
 def main():
 
     # prova('./Dataset_Artista/Sesso_Staccato.png')
-    prova2('./Dataset_Artista/Sesso_Duro.png')
+    monkeyRead('./Dataset_Artista/Sesso_Duro.png')
     '''
     # prepara il dataset
     data_train, data_test, label_train, label_test = prepare_data('trimmedData.csv')
